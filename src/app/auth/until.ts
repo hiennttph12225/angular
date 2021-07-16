9b5dbd4740b32326545fb7fd914644f1813c1fef
lequyet2001 <lequyet840500@gmail.com>
2021-07-16 17:48:33 +0700

    update

--- src/app/auth/until.ts
+++ src/app/auth/until.ts
@@ -3,6 +3,9 @@ import { localStoragetype } from "../type/signinTypes"
 const user : localStoragetype = JSON.parse(localStorage.getItem("user")!)
 
 export const isToken = () =>{
+      if (user.token == undefined) {
+            return localStorage.setItem("user", JSON.stringify({}))
+      }
       return user.token
 }
 export const isUserId = () =>{
